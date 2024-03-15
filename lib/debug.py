6251f7a9e786e0940c07129bb71003f158c00591
Robins <robinmgk077@gmail.com>
2024-03-15 23:31:21 +0300

    Completed Assignment

--- lib/debug.py
+++ lib/debug.py
@@ -5,15 +5,16 @@ import random
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from models import Game
+from models import Game, Base
 
 fake = Faker()
 
 if __name__ == '__main__':
-    
+
     engine = create_engine('sqlite:///seed_db.db')
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-
-    import ipdb; ipdb.set_trace()
+    import ipdb
+    ipdb.set_trace()
